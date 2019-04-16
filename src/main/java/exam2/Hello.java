f1177a42e54fe1c20e6bed6a48633562d99ccdca
TjyLx <1148770669@qq.com>
2019-04-16 12:22:24 +0800

    第一次

---
+++ src/main/java/exam2/Hello.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package exam2;
+
+/**
+ * @Master of mountains,guardian of the Sky,The king of Wizard ———— Lee
+ *
+ * 2019年4月12日
+ */
+public class Hello {
+
+	
+	public void adsd() {
+		System.out.println("asd");
+	}
+}
